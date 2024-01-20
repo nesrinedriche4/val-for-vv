4531f35503a72839c80f6c6cd9a9319a10236b4a
nesrinedriche4 <157232734+nesrinedriche4@users.noreply.github.com>
2024-01-20 17:47:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function response(answer) {
+    if (answer === 'yes') {
+        alert("Meerciiii for making me happyyy, muaaaah!");
+    } else if (answer === 'no') {
+        alert("wrong answer, I'm sad now :(");
+    }
+}
